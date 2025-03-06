46a7399891372d94a827c72ac975cff01004a4ac
chiluveripavan <pavanchiluveri74@gmail.com>
2025-03-06 23:15:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "one" {
-  ami           = "ami-05b10e08d247fb927"
+  ami           = "ami-02a53b0d62d37a757"
   instance_type = "t2.micro"
   tags = {
     Name = "hcp-server"
